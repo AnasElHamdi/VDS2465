9cea50e9002dc65686ca9749906e372ba34f53e0
AnasElHamdi <104059940+AnasElHamdi@users.noreply.github.com>
2022-11-15 19:06:28 +0100

    Update main-esi.py

--- main-esi.py
+++ main-esi.py
@@ -5,19 +5,19 @@ import time
 
 class Que():
     def Warteschlange(self,):
-#definiren einer Liste
+#definieren einer Liste
         q = deque
         q.append = ('0000')
 
-#versuch Object chars zu übergeben nur als test
+#versuch Object chars zu übergeben als test
        # q.append = object.ID_chars
 
         print(q)
 
-#Que leeren nachdem sie benutz wurde
+#Que leeren nachdem sie benutz wurde damit sie wieder befüllt werden kann
         (q.popleft())
         print(q)
-
+#Starten der vds 
 vds = VDS()
 
 
