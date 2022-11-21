058808cf3732e82a14d641325af6989375e2de85
AnasElHamdi <104059940+AnasElHamdi@users.noreply.github.com>
2022-11-21 13:31:24 +0100

    Update main-esi.py

--- main-esi.py
+++ main-esi.py
@@ -3,21 +3,10 @@ from collections import deque
 from vds import VDSpackage
 import time
 
-class Que():
-    def Warteschlange(self,):
-#definieren einer Liste
-        q = deque
-        q.append = ('0000')
 
-#versuch Object chars zu übergeben als test
-       # q.append = object.ID_chars
 
-        print(q)
 
-#Que leeren nachdem sie benutz wurde damit sie wieder befüllt werden kann
-        (q.popleft())
-        print(q)
-#Starten der vds 
+
 vds = VDS()
 
 
