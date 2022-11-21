cb7c927eaa9d773ace9a4a291aa977beedda62e9
AnasElHamdi <104059940+AnasElHamdi@users.noreply.github.com>
2022-11-21 15:11:12 +0100

    Update main-esi.py

--- main-esi.py
+++ main-esi.py
@@ -1,10 +1,12 @@
 from vds import VDS
+from vds import data
+#from vds import Warteschlange
 from collections import deque
 from vds import VDSpackage
 import time
 
 
-
+data = data("0000")
 
 
 vds = VDS()
